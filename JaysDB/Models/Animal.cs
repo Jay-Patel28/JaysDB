e1570be00a5afc196322b30010be6251197e1756
Jay Patel <Jay Patel@DESKTOP-A8TJ7KI>
2022-09-02 18:00:48 +0530

    Add project files.

---
+++ JaysDB/Models/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JaysDB.Models
+{
+    public partial class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Weight { get; set; }
+        public string City { get; set; } = null!;
+    }
+}
---
+++ JaysDB/Models/JPFORESTContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace JaysDB.Models
+{
+    public partial class JPFORESTContext : DbContext
+    {
+        public JPFORESTContext()
+        {
+        }
+
+        public JPFORESTContext(DbContextOptions<JPFORESTContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Animal> Animals { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Animal>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.City)
+                    .HasMaxLength(30)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(30)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
